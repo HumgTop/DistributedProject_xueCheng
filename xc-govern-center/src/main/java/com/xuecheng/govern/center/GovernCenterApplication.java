05853ca5f8bb9da28c2be24974c822ad5b4fd427
NUC <me@humg.top>
2021-05-12 08:57:13 +0800

    Eureka注册中心
    实现服务治理功能

---
+++ xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
@@ -0,0 +1,18 @@
+package com.xuecheng.govern.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Autor HumgTop
+ * @Date 2021/5/11 20:38
+ * @Version 1.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class GovernCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GovernCenterApplication.class, args);
+    }
+}
--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
@@ -3,12 +3,14 @@ package com.xuecheng.manage_course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Administrator
  * @version 1.0
  **/
+@EnableDiscoveryClient  //Eureka客户端注解
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.course")//扫描实体类
 @ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
